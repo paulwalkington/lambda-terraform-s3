61cb65db99d124339c7235a7ba48bcb22f06e5d3
Paul Walkington <paul.walkington@infinityworks.com>
2021-01-06 14:50:06 +0000

    tried to add second lambda to handle messages from sqs

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,8 +2,8 @@ provider "aws" {
   region = "eu-west-1"
 }
 
-resource "aws_iam_policy" "policy" {
-  name        = "paul_lambda_policy"
+resource "aws_iam_policy" "gateway_lambda_policy" {
+  name        = "paul_gateway_lambda_policy"
   path        = "/"
   description = "My test policy"
 
@@ -26,14 +26,47 @@ resource "aws_iam_policy" "policy" {
                 "s3:PutObject"
             ],
             "Resource": "arn:aws:s3:::paul-test-bucket-random/*"
-        }
+          }
         ]
       }
 EOF
 }
 
-resource "aws_iam_role" "role" {
-  name = "paul_role_lambda_test"
+resource "aws_iam_policy" "sqs_lambda_policy" {
+  name        = "paul_sqs_lambda_policy"
+  path        = "/"
+  description = "My test policy"
+
+  policy = <<EOF
+{
+        "Version": "2012-10-17",
+        "Statement": [
+          {
+            "Effect": "Allow",
+            "Action": [
+              "logs:CreateLogGroup",
+              "logs:CreateLogStream",
+              "logs:PutLogEvents",
+              "sqs:ReceiveMessage",
+              "sqs:DeleteMessage",
+              "sqs:GetQueueAttributes"
+            ],
+            "Resource": "*"
+          },
+          {
+            "Effect": "Allow",
+            "Action": [
+                "s3:PutObject"
+            ],
+            "Resource": "arn:aws:s3:::paul-test-bucket-random/*"
+          }
+        ]
+      }
+EOF
+}
+
+resource "aws_iam_role" "gateway_lambda_role" {
+  name = "paul_gateway_role_lambda_test"
 
   assume_role_policy = <<EOF
 {
@@ -52,21 +85,66 @@ resource "aws_iam_role" "role" {
 EOF
 }
 
-resource "aws_iam_role_policy_attachment" "test-attach" {
-  role       = aws_iam_role.role.name
-  policy_arn = aws_iam_policy.policy.arn
+resource "aws_iam_role" "sqs_lambda_role" {
+  name = "paul_sqs_role_lambda_test"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
 }
 
-resource "aws_lambda_function" "test_lambda" {
-  filename      = "../lambda-s3/lambda_function.zip"
-  function_name = "paul-test-lambda"
-  role          = aws_iam_role.role.arn
-  handler       = "lambda_function.lambda_handler"
+resource "aws_iam_role_policy_attachment" "gateway_role_policy_attach" {
+  role       = aws_iam_role.gateway_lambda_role.name
+  policy_arn = aws_iam_policy.gateway_lambda_policy.arn
+}
+
+resource "aws_iam_role_policy_attachment" "sqs_role_policy_attach" {
+  role       = aws_iam_role.sqs_lambda_role.name
+  policy_arn = aws_iam_policy.sqs_lambda_policy.arn
+}
+
+resource "aws_lambda_function" "gateway_lambda" {
+  filename      = "../lambda-s3/gateway_lambda_function.zip"
+  function_name = "paul-gateway-test-lambda"
+  role          = aws_iam_role.gateway_lambda_role.arn
+  handler       = "gateway_lambda_function.lambda_handler"
 
   # The filebase64sha256() function is available in Terraform 0.11.12 and later
   # For Terraform 0.11.11 and earlier, use the base64sha256() function and the file() function:
   # source_code_hash = "${base64sha256(file("lambda_function.zip"))}"
-  source_code_hash = filebase64sha256("../lambda-s3/lambda_function.zip")
+  source_code_hash = filebase64sha256("../lambda-s3/gateway_lambda_function.zip")
+
+  runtime = "python3.8"
+
+  environment {
+    variables = {
+      foo = "bar"
+    }
+  }
+}
+
+resource "aws_lambda_function" "sqs_lambda" {
+  filename      = "../lambda-s3/sqs_lambda_function.zip"
+  function_name = "paul-sqs-test-lambda"
+  role          = aws_iam_role.sqs_lambda_role.arn
+  handler       = "sqs_lambda_function.lambda_handler"
+
+  # The filebase64sha256() function is available in Terraform 0.11.12 and later
+  # For Terraform 0.11.11 and earlier, use the base64sha256() function and the file() function:
+  # source_code_hash = "${base64sha256(file("lambda_function.zip"))}"
+  source_code_hash = filebase64sha256("../lambda-s3/sqs_lambda_function.zip")
 
   runtime = "python3.8"
 
@@ -90,6 +168,19 @@ resource "aws_s3_bucket" "s3_bucket" {
 module "api-gateway" {
   source = "./modules/api-gateway"
 
-  lambda_arn = aws_lambda_function.test_lambda.invoke_arn
-  lambda_function_name = aws_lambda_function.test_lambda.function_name
+  lambda_arn = aws_lambda_function.gateway_lambda.invoke_arn
+  lambda_function_name = aws_lambda_function.gateway_lambda.function_name
+}
+
+resource "aws_sqs_queue" "sqs_queue_test" {
+  name                      = "paul-example-queue"
+  delay_seconds             = 90
+  max_message_size          = 2048
+  message_retention_seconds = 86400
+  receive_wait_time_seconds = 10
+}
+
+resource "aws_lambda_event_source_mapping" "source_mapping_example" {
+  event_source_arn = aws_sqs_queue.sqs_queue_test.arn
+  function_name    = aws_lambda_function.sqs_lambda.arn
 }
